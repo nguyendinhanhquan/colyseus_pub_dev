d3b5f7b6897e632856f8bd85f842b7a886694801
AnhQuan <nguyendinhanhquan0305@gmail.com>
2022-09-28 22:10:26 +0700

    update state

--- src/rooms/02-state-handler.ts
+++ src/rooms/02-state-handler.ts
@@ -1,12 +1,14 @@
 import { Room, Client } from "colyseus";
-import { Schema, type, MapSchema } from "@colyseus/schema";
+import { Schema, Context, MapSchema } from "@colyseus/schema";
+
+const type = Context.create();
 
 export class Player extends Schema {
     @type("number")
-    x = Math.floor(Math.random() * 400);
+    x = 0.0;
 
     @type("number")
-    y = Math.floor(Math.random() * 400);
+    y = 0.0;
 }
 
 export class State extends Schema {
@@ -14,6 +16,7 @@ export class State extends Schema {
     players = new MapSchema<Player>();
 
     something = "This attribute won't be sent to the client-side";
+
 
     createPlayer(sessionId: string) {
         this.players.set(sessionId, new Player());
@@ -23,7 +26,7 @@ export class State extends Schema {
         this.players.delete(sessionId);
     }
 
-    movePlayer (sessionId: string, movement: any) {
+    movePlayer(sessionId: string, movement: any) {
         if (movement.x) {
             this.players.get(sessionId).x += movement.x * 10;
 
@@ -36,7 +39,7 @@ export class State extends Schema {
 export class StateHandlerRoom extends Room<State> {
     maxClients = 4;
 
-    onCreate (options) {
+    onCreate(options) {
         console.log("StateHandlerRoom created!", options);
 
         this.setState(new State());
@@ -51,16 +54,16 @@ export class StateHandlerRoom extends Room<State> {
         return true;
     }
 
-    onJoin (client: Client) {
+    onJoin(client: Client) {
         client.send("hello", "world");
         this.state.createPlayer(client.sessionId);
     }
 
-    onLeave (client) {
+    onLeave(client) {
         this.state.removePlayer(client.sessionId);
     }
 
-    onDispose () {
+    onDispose() {
         console.log("Dispose StateHandlerRoom");
     }
 
