c30c879937c64981c0b587a8a2ff1721f664afce
AnhQuan <nguyendinhanhquan0305@gmail.com>
2022-10-25 21:14:08 +0700

    update test source

--- src/rooms/01-chat-room.ts
+++ src/rooms/01-chat-room.ts
@@ -4,24 +4,24 @@ export class ChatRoom extends Room {
     // this room supports only 4 clients connected
     maxClients = 4;
 
-    onCreate (options) {
+    onCreate(options) {
         console.log("ChatRoom created!", options);
 
         this.onMessage("message", (client, message) => {
-            console.log("ChatRoom received message from", client.sessionId, ":", message);
+            console.log("ChatRoom received message from 123", client.sessionId, ":", message);
             this.broadcast("messages", `(${client.sessionId}) ${message}`);
         });
     }
 
-    onJoin (client) {
-        this.broadcast("messages", `${ client.sessionId } joined.`);
+    onJoin(client) {
+        this.broadcast("messages", `${client.sessionId} joined.`);
     }
 
-    onLeave (client) {
-        this.broadcast("messages", `${ client.sessionId } left.`);
+    onLeave(client) {
+        this.broadcast("messages", `${client.sessionId} left.`);
     }
 
-    onDispose () {
+    onDispose() {
         console.log("Dispose ChatRoom");
     }
 
